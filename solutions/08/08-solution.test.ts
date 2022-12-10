71b587ddb4ffe4c65badf3428ec496896ba7be52
Antonin Vogt <antoninvogt@users.noreply.github.com>
2022-12-10 02:22:50 +0100

    add solution to day 08, part 2

--- solutions/08/08-solution.test.ts
+++ solutions/08/08-solution.test.ts
@@ -1,10 +1,16 @@
 import {
+	MapCoordinates,
+	areCoordinatesInBounds,
+	getLinearMapTraversalCoordinates,
 	determineVisibilityByRow,
-	determineVisibilityByColumn
+	determineVisibilityByColumn,
+	determineViewDistance,
+	determineScenicScores,
+	searchForHighestScenicScore
 } from "./08-solution";
 
 describe("Day 08", () => {
-	const treeRows = [
+	const treeRowsStrings = [
 		"30373",
 		"25512",
 		"65332",
@@ -12,9 +18,276 @@ describe("Day 08", () => {
 		"35390"
 	];
 
+	describe("areCoordinatesInBounds", () => {
+		it("should identify which coordinate are in or out of bounds", () => {
+			const map = [
+				[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+				[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+				[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+				[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+				[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+			];
+			const exampleCoordinatesList = [
+				[ 0, 4 ],
+				[ 4, 0 ],
+				[ -1, 4 ],
+				[ 4, -1 ],
+				[ 0, 5 ],
+				[ 5, 0 ]
+			] as MapCoordinates[];
+
+			const result = exampleCoordinatesList.map((exampleCoordinates) => areCoordinatesInBounds(map, exampleCoordinates));
+
+			expect(result).toEqual([
+				true,
+				true,
+				false,
+				false,
+				false,
+				false
+			]);
+		});
+	});
+
+	describe("getLinearMapTraversalCoordinates", () => {
+		describe("direction: row, forward", () => {
+			const direction = "ROW_FORWARD";
+
+			it("should generate the appropriate full sequence for the given row", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 0 ],
+					[ 1, 1 ],
+					[ 1, 2 ],
+					[ 1, 3 ],
+					[ 1, 4 ]
+				]);
+			});
+
+			it("should generate the appropriate sequence starting from the given point", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const startCol = 2;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 2 ],
+					[ 1, 3 ],
+					[ 1, 4 ]
+				]);
+			});
+		});
+
+		describe("direction: row, reverse", () => {
+			const direction = "ROW_REVERSE";
+
+			it("should generate the appropriate full sequence for the given row", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 4 ],
+					[ 1, 3 ],
+					[ 1, 2 ],
+					[ 1, 1 ],
+					[ 1, 0 ]
+				]);
+			});
+
+			it("should generate the appropriate sequence starting from the given point", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const startCol = 2;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 2 ],
+					[ 1, 1 ],
+					[ 1, 0 ]
+				]);
+			});
+		});
+		describe("direction: row, forward", () => {
+			const direction = "ROW_FORWARD";
+
+			it("should generate the appropriate full sequence for the given row", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 0 ],
+					[ 1, 1 ],
+					[ 1, 2 ],
+					[ 1, 3 ],
+					[ 1, 4 ]
+				]);
+			});
+
+			it("should generate the appropriate sequence starting from the given point", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 1;
+				const startCol = 2;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 1, 2 ],
+					[ 1, 3 ],
+					[ 1, 4 ]
+				]);
+			});
+		});
+
+		describe("direction: col, forward", () => {
+			const direction = "COL_FORWARD";
+
+			it("should generate the appropriate full sequence for the given column", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startCol = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, undefined, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 0, 1 ],
+					[ 1, 1 ],
+					[ 2, 1 ],
+					[ 3, 1 ],
+					[ 4, 1 ]
+				]);
+			});
+
+			it("should generate the appropriate sequence starting from the given point", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 2;
+				const startCol = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 2, 1 ],
+					[ 3, 1 ],
+					[ 4, 1 ]
+				]);
+			});
+		});
+
+		describe("direction: col, reverse", () => {
+			const direction = "COL_REVERSE";
+
+			it("should generate the appropriate full sequence for the given column", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startCol = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, undefined, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 4, 1 ],
+					[ 3, 1 ],
+					[ 2, 1 ],
+					[ 1, 1 ],
+					[ 0, 1 ]
+				]);
+			});
+
+			it("should generate the appropriate sequence starting from the given point", () => {
+				const map = [
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ],
+					[ Symbol(), Symbol(), Symbol(), Symbol(), Symbol() ]
+				];
+				const startRow = 2;
+				const startCol = 1;
+				const linearMapTraversalCoordinates = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+
+				const result = [...linearMapTraversalCoordinates];
+
+				expect(result).toEqual([
+					[ 2, 1 ],
+					[ 1, 1 ],
+					[ 0, 1 ]
+				]);
+			});
+		});
+	});
+
 	describe("determineVisibilityByRow", () => {
 		it("should mark all trees on the outer ends as visible by default", () => {
-			const result = determineVisibilityByRow(treeRows, new Set<string>());
+			const result = determineVisibilityByRow(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.arrayContaining([
 				"row0col0",
@@ -31,7 +304,7 @@ describe("Day 08", () => {
 		});
 
 		it("should mark all inner trees which are visible from either side horizontally", () => {
-			const result = determineVisibilityByRow(treeRows, new Set<string>());
+			const result = determineVisibilityByRow(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.arrayContaining([
 				"row1col1",
@@ -43,7 +316,7 @@ describe("Day 08", () => {
 		});
 
 		it("should not mark any inner trees which are not visible from either side horizontally", () => {
-			const result = determineVisibilityByRow(treeRows, new Set<string>());
+			const result = determineVisibilityByRow(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.not.arrayContaining([
 				"row1col3",
@@ -56,7 +329,7 @@ describe("Day 08", () => {
 
 	describe("determineVisibilityByColumn", () => {
 		it("should mark all trees on the outer ends as visible by default", () => {
-			const result = determineVisibilityByColumn(treeRows, new Set<string>());
+			const result = determineVisibilityByColumn(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.arrayContaining([
 				"row0col0",
@@ -73,7 +346,7 @@ describe("Day 08", () => {
 		});
 
 		it("should mark all inner trees which are visible from either side horizontally", () => {
-			const result = determineVisibilityByColumn(treeRows, new Set<string>());
+			const result = determineVisibilityByColumn(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.arrayContaining([
 				"row1col1",
@@ -83,7 +356,7 @@ describe("Day 08", () => {
 		});
 
 		it("should not mark any inner trees which are not visible from either side horizontally", () => {
-			const result = determineVisibilityByColumn(treeRows, new Set<string>());
+			const result = determineVisibilityByColumn(treeRowsStrings, new Set<string>());
 
 			expect([ ...result ]).toEqual(expect.not.arrayContaining([
 				"row1col3",
@@ -93,6 +366,83 @@ describe("Day 08", () => {
 				"row3col1",
 				"row3col3"
 			]));
+		});
+	});
+
+	describe("determineViewDistance", () => {
+		const treeRows = [
+			[ 3, 0, 3, 7, 3 ],
+			[ 2, 5, 5, 1, 2 ],
+			[ 6, 5, 3, 3, 2 ],
+			[ 3, 3, 5, 4, 9 ],
+			[ 3, 5, 3, 9, 0 ]
+		];
+
+		it("should determine the appropriate view distances in each direction for element [1;2] from the puzzle example", () => {
+			const coordinates = [ 1, 2 ] as MapCoordinates;
+			const result = [
+				determineViewDistance(treeRows, coordinates, "COL_REVERSE"),
+				determineViewDistance(treeRows, coordinates, "ROW_FORWARD"),
+				determineViewDistance(treeRows, coordinates, "COL_FORWARD"),
+				determineViewDistance(treeRows, coordinates, "ROW_REVERSE")
+			];
+
+			expect(result).toEqual([
+				1,
+				2,
+				2,
+				1
+			]);
+		});
+
+		it("should determine the appropriate view distances in each direction for element [3;2] from the puzzle example", () => {
+			const coordinates = [ 3, 2 ] as MapCoordinates;
+			const result = [
+				determineViewDistance(treeRows, coordinates, "COL_REVERSE"),
+				determineViewDistance(treeRows, coordinates, "ROW_FORWARD"),
+				determineViewDistance(treeRows, coordinates, "COL_FORWARD"),
+				determineViewDistance(treeRows, coordinates, "ROW_REVERSE")
+			];
+
+			expect(result).toEqual([
+				2,
+				2,
+				1,
+				2
+			]);
+		});
+	});
+
+	describe("determineScenicScores", () => {
+		const treeRows = [
+			[ 3, 0, 3, 7, 3 ],
+			[ 2, 5, 5, 1, 2 ],
+			[ 6, 5, 3, 3, 2 ],
+			[ 3, 3, 5, 4, 9 ],
+			[ 3, 5, 3, 9, 0 ]
+		];
+
+		it("should determine the scenic score for each tree in the map", () => {
+			const result = determineScenicScores(treeRows);
+
+			expect(result[1][2]).toEqual(4);
+			expect(result[3][2]).toEqual(8);
+		});
+	});
+
+	describe("searchForHighestScenicScore", () => {
+		it("should determine the scenic score for each tree in the map", () => {
+			const scenicScoresMap = [
+				[ 0, 0, 0, 0, 0 ],
+				[ 0, 1, 4, 1, 0 ],
+				[ 0, 6, 1, 2, 0 ],
+				[ 0, 1, 8, 3, 0 ],
+				[ 0, 0, 0, 0, 0 ]
+			];
+
+			const result = searchForHighestScenicScore(scenicScoresMap);
+
+			expect(result).toEqual(8);
 		});
 	});
 });
--- solutions/08/08-solution.ts
+++ solutions/08/08-solution.ts
@@ -1,13 +1,86 @@
 import * as inputHelper from "../../helpers/input";
 
-type TreeMapRow = string;
+type MapRow<T> = T[];
+type Map<T> = MapRow<T>[];
+type MapCoordinates = [ rowIndex: number, columnIndex: number ];
+type MapTraversalDirection = ("ROW_FORWARD"|"ROW_REVERSE"|"COL_FORWARD"|"COL_REVERSE");
+
+type TreeMapRowString = string;
+type TreeMapRow = number[];
+type TreeMap = TreeMapRow[];
 type KnownVisibleTrees = Set<string>; // format: "row12col34"
+
+function areCoordinatesInBounds<T>(map: Map<T>, coordinates: MapCoordinates): boolean {
+	const [ rowIndex, colIndex ] = coordinates;
+
+	return (rowIndex >= 0 && rowIndex < map.length && colIndex >= 0 && colIndex < map[0].length);
+}
+
+function* getLinearMapTraversalCoordinates<T>(map: Map<T>, direction: MapTraversalDirection, startRowIndex?: number, startColIndex?: number): Generator<MapCoordinates> {
+	if (!map.length || !map?.[0].length) {
+		throw new Error("Rows and columns may not be empty.");
+	}
+	if (direction.startsWith("ROW") && startRowIndex === undefined) {
+		throw new Error("A start row must be specified for this direction.");
+	}
+	if (direction.startsWith("COL") && startColIndex === undefined) {
+		throw new Error("A start column must be specified for this direction.");
+	}
+
+	const defaultStartRow: Record<MapTraversalDirection, number> = {
+		ROW_FORWARD: -1,
+		ROW_REVERSE: -1,
+		COL_FORWARD: 0,
+		COL_REVERSE: map.length - 1
+	};
+
+	const defaultStartCol: Record<MapTraversalDirection, number> = {
+		ROW_FORWARD: 0,
+		ROW_REVERSE: map[0].length - 1,
+		COL_FORWARD: -1,
+		COL_REVERSE: -1
+	};
+
+	let currentCoordinates = [ startRowIndex ?? defaultStartRow[direction], startColIndex ?? defaultStartCol[direction] ] as MapCoordinates;
+
+	while (areCoordinatesInBounds<T>(map, currentCoordinates)) {
+		yield currentCoordinates;
+
+		const [ currentRow, currentCol ] = currentCoordinates;
+		let nextRow: number, nextCol: number;
+
+		switch (direction) {
+			case "ROW_FORWARD":
+				nextRow = currentRow;
+				nextCol = currentCol + 1;
+
+				break;
+			case "ROW_REVERSE":
+				nextRow = currentRow;
+				nextCol = currentCol - 1;
+
+				break;
+			case "COL_FORWARD":
+				nextRow = currentRow + 1;
+				nextCol = currentCol;
+
+				break;
+			case "COL_REVERSE":
+				nextRow = currentRow - 1;
+				nextCol = currentCol;
+
+				break;
+		}
+
+		currentCoordinates = [ nextRow, nextCol ];
+	}
+}
 
 function addKnownVisibleTree(knownVisibleTrees: KnownVisibleTrees, rowIndex: number, columnIndex: number): void {
 	knownVisibleTrees.add(`row${rowIndex}col${columnIndex}`);
 }
 
-function determineVisibilityByRow(rows: TreeMapRow[], knownVisibleTrees: KnownVisibleTrees): KnownVisibleTrees {
+function determineVisibilityByRow(rows: TreeMapRowString[], knownVisibleTrees: KnownVisibleTrees): KnownVisibleTrees {
 	for (let rowIndex = 0; rowIndex < rows.length; rowIndex++) {
 		const row = rows[rowIndex];
 		let highestTreeFromLeft = -1;
@@ -44,7 +117,7 @@ function determineVisibilityByRow(rows: TreeMapRow[], knownVisibleTrees: KnownVi
 	return knownVisibleTrees;
 }
 
-function determineVisibilityByColumn(rows: TreeMapRow[], knownVisibleTrees: KnownVisibleTrees): KnownVisibleTrees {
+function determineVisibilityByColumn(rows: TreeMapRowString[], knownVisibleTrees: KnownVisibleTrees): KnownVisibleTrees {
 	for (let columnIndex = 0; columnIndex < rows[0].length; columnIndex++) {
 		let highestTreeFromTop = -1;
 		let highestTreeFromBottom = -1;
@@ -82,6 +155,59 @@ function determineVisibilityByColumn(rows: TreeMapRow[], knownVisibleTrees: Know
 	return knownVisibleTrees;
 }
 
+function createTreeMapFromInputStrings(treeMapRows: TreeMapRowString[]): TreeMap {
+	return treeMapRows.map((treeMapRow) => treeMapRow.split("").map(Number));
+}
+
+function determineViewDistance(map: TreeMap, startCoordinates: MapCoordinates, direction: MapTraversalDirection): number {
+	const [ startRow, startCol ] = startCoordinates;
+	const traversalSteps = getLinearMapTraversalCoordinates(map, direction, startRow, startCol);
+	const originTreeHeight = map[startRow][startCol];
+	let viewDistance = 0;
+
+	for (let stepCoordinates of traversalSteps) {
+		const [ nextRow, nextCol ] = stepCoordinates;
+
+		if (startRow === nextRow && startCol === nextCol) {
+			continue; // skipping the first item, because the steps contain the current coordinates - TODO there's probably a more elegant way to do this
+		}
+
+		viewDistance++;
+
+		if (map[nextRow][nextCol] >= originTreeHeight) {
+			break;
+		}
+	}
+
+	return viewDistance;
+}
+
+function determineScenicScores(treeMap: TreeMap): Map<number> {
+	return treeMap.map((treeMapRow, rowIndex) => {
+		return treeMapRow.map((_, columnIndex) => {
+			const viewDistanceChecker = determineViewDistance.bind(null, treeMap, [ rowIndex, columnIndex ]);
+
+			return (
+				viewDistanceChecker("ROW_FORWARD") *
+				viewDistanceChecker("ROW_REVERSE") *
+				viewDistanceChecker("COL_FORWARD") *
+				viewDistanceChecker("COL_REVERSE")
+			);
+		});
+	});
+}
+
+function searchForHighestScenicScore(scenicScoresMap: Map<number>): number {
+	return scenicScoresMap
+		.reduce((highestScore, mapRow) => {
+			const highestRowScore = mapRow.reduce((currentHighestRowScore, scenicScore) => {
+				return Math.max(currentHighestRowScore, scenicScore);
+			}, 0);
+
+			return Math.max(highestScore, highestRowScore);
+		}, 0);
+}
+
 async function solve(): Promise<string[]> {
 	const puzzleInput = await inputHelper.readPuzzleInput(__dirname, "./08-input.txt");
 
@@ -92,13 +218,24 @@ async function solve(): Promise<string[]> {
 
 	const numberOfVisibleTrees = knownVisibleTrees.size;
 
+	const treeMap = createTreeMapFromInputStrings(puzzleInput);
+	const scenicScores = determineScenicScores(treeMap);
+	const highestScenicScore = searchForHighestScenicScore(scenicScores);
+
 	return [
-		String(numberOfVisibleTrees)
+		String(numberOfVisibleTrees),
+		String(highestScenicScore)
 	];
 }
 
 export {
+	MapCoordinates,
+	areCoordinatesInBounds,
+	getLinearMapTraversalCoordinates,
 	determineVisibilityByRow,
 	determineVisibilityByColumn,
+	determineViewDistance,
+	determineScenicScores,
+	searchForHighestScenicScore,
 	solve
 }
